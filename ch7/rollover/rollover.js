e7e487f511c379717a55e10b5468751928d82679
yohanajovani <yohanamehari2050@gmail.com>
2023-10-09 21:39:30 +0000

    ch7

---
+++ ch7/rollover/rollover.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const $ = selector => document.querySelector(selector);
+
+document.addEventListener("DOMContentLoaded", () => {
+    var image1 = $("#image1");
+    var image2 = $("#image2");
+
+    // preload images
+    var links = $("#image_list").querySelectorAll("a");
+
+    for (let link of links) {
+        const image = new Image();
+        image.src = link.href;
+    }
+
+    for ( let link of links) {
+        image1.addEventListener("mouseover", () => {
+            image1.src = "./images/release.jpg";
+        });
+        image1.addEventListener("mouseout", () => {
+            image1.src = "./images/hero.jpg";
+        });
+        image2.addEventListener("mouseover", () => {
+            image2.src = "./images/deer.jpg";
+        });
+        image2.addEventListener("mouseout", () => {
+            image2.src = "./images/bison.jpg";
+        });           
+    }
+
+});
